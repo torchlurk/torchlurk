cd82aa182f281b098d5d3878de52030782e707fd
Yann Mentha <yann.mentha@gmail.com>
2020-05-12 21:57:16 +0200

    server embedded in lurker

--- .ipynb_checkpoints/app-checkpoint.py
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -7,6 +7,10 @@ import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
 import sys
+import getopt
+
+short_options = "p:"
+long_options = ["port="]
 sys.path.insert(1,"./src")
 
 # App definition
@@ -18,4 +22,16 @@ def home():
     return render_template('main.html')
 
 if __name__ == "__main__":
-    app.run(debug=True,host= '0.0.0.0',port=5000)
+    port = None
+    try:
+        arguments, values = getopt.getopt(sys.argv[1:], short_options, long_options)
+    except getopt.error as err:
+        # Output error, and return with an error code
+        print (str(err))
+        sys.exit(2)
+    for curr_arg,curr_val in arguments:
+        if curr_arg in ("-p","--port"):
+            port = curr_val
+    if port is None:
+        port = 5000
+    app.run(debug=True,host= '0.0.0.0',port=port)
